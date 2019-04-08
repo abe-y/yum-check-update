22f14f18336aa25ca76a4c61b90524b02809d362
Yusuke Abe <37325616+abe-y@users.noreply.github.com>
2019-04-08 17:04:38 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ pushd $TMP_DIR
 git clone git@github.com:abe-y/yum-check-update.git
 cp -r yum-check-update /usr/local/
 popd $TMP_DIR
+rm -rf $TMP_DIR
 
 echo '[plugin.metrics.yum-check-update]
 command = "/usr/local/yum-check-update/bin/yum-check-update"
