c53857bd988eb89bac03762ef1f387855e8fd564
Yusuke Abe <abe-y@kamakura-net.co.jp>
2019-04-08 07:49:50 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # yum-check-update
+#  mackerel-agent plugin 
+
+# install 
+curl -fsSL https://raw.githubusercontent.com/abe-y/yum-check-update/master/install.sh | sudo bash
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TMP_DIR="tmp.$(date +'%s')"
+mkdir $TMP_DIR
+pushd $TMP_DIR
+git clone git@github.com:abe-y/yum-check-update.git
+cp -r yum-check-update /usr/local/
+popd $TMP_DIR
+
+echo '[plugin.metrics.yum-check-update]
+command = "/usr/local/yum-check-update/bin/yum-check-update"
+' >> /etc/mackerel-agent/mackerel-agent.conf
+service mackerel-agent restart
