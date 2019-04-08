3d1a2aebcc0f5b4edbd4c5abf27139c8ecbabfb5
Yusuke Abe <37325616+abe-y@users.noreply.github.com>
2019-04-08 17:28:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ git clone https://github.com/abe-y/yum-check-update.git
 cp -r yum-check-update /usr/local/
 popd ./tmp.$DATE
 
-echo '[plugin.metrics.yum-check-update]
+if [ "$(grep yum-check-update /etc/mackerel-agent/mackerel-agent.conf)" = "" ];then 
+ echo '[plugin.metrics.yum-check-update]
 command = "/usr/local/yum-check-update/bin/yum-check-update"
 ' >> /etc/mackerel-agent/mackerel-agent.conf
-service mackerel-agent restart
+ service mackerel-agent restart
+fi
