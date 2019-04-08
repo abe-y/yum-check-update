bc2e95d6bba500f6be62ee4889005e5df11edb42
Yusuke Abe <37325616+abe-y@users.noreply.github.com>
2019-04-08 17:08:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DATE="$(date +'%s')"
-mkdir tmp.$DATE
-pushd tmp.$DATE
+mkdir ./tmp.$DATE
+pushd ./tmp.$DATE
 git clone https://github.com/abe-y/yum-check-update.git
 cp -r yum-check-update /usr/local/
-popd tmp.$DATE
-rm -rf tmp.$DATE
+popd ./tmp.$DATE
+rm -rf ./tmp.$DATE
 
 echo '[plugin.metrics.yum-check-update]
 command = "/usr/local/yum-check-update/bin/yum-check-update"
